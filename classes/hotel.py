1a4de9620582876787293799e7dd506c781364b4
César <up202305778@up.pt>
2024-04-12 19:14:16 +0100

    att aula 12/4 e pasta data

--- classes/hotel.py
+++ classes/hotel.py
@@ -8,12 +8,13 @@ class Hotel(Gclass):
     sortkey = ''
     autonumber = 0
     nkey = 1
-    def __init__(self, codigo, nquartos, contacto):
+    def __init__(self, codigo, localizacao, nquartos, contacto, nfuncionarios):
         super().__init__()  
         self._codigo = codigo
         self._nquartos = nquartos
+        self._localizacao = localizacao
         self._contacto = contacto
-        
+        self._nfuncionarios = nfuncionarios
         Hotel.lst.append(self)
         Hotel.obj[codigo] = self
         
@@ -24,6 +25,12 @@ class Hotel(Gclass):
     def nquartos(self):
         return self._nquartos
     @property 
+    def localizacao(self):
+        return self._localizacao
+    @property 
     def contacto(self):
         return self._contacto 
-        
+    @property 
+    def nfuncionarios(self):
+        return self._nfuncionarios
+    
