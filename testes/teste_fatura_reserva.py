00e25076e3e88cd688ea0a33fcc8ec650bcb117a
VicenteRilo <up202208447@fe.up.pt>
2024-05-03 11:01:06 +0100

    testes fatura reserva

---
+++ testes/teste_fatura_reserva.py
@@ -0,0 +1,42 @@
+
+from datafile import filename
+
+from classes.hotel import Hotel
+from classes.quarto import Quarto
+from classes.reservaquarto import ReservaQuarto
+from classes.fatura import Fatura
+from classes.fatura_reserva import Fatura_Reserva
+
+Hotel.read(filename + 'hotel.db')
+Quarto.read(filename + 'hotel.db')
+ReservaQuarto.read(filename + 'hotel.db')
+Fatura_Reserva.read(filename + 'hotel.db')
+Fatura.read(filename + 'hotel.db')
+
+cname = "ReservaQuarto"
+cl = eval(cname)
+
+obj = cl.from_string('rq1;2-01-2024;2-01-2024;Ativa;A100;12345')
+
+print("objeto sem estar gravado ",obj)
+
+cl.insert(getattr(obj,cl.att[0]))
+
+obj = cl.from_string('rq1;2-01-2024;2-01-2024;Ativa;A100;12345')
+cl.insert(getattr(obj,cl.att[0]))
+
+
+print("\nLista dos onjetos gravados " ,cl.lst)
+
+
+# alterar
+obj = cl.first()
+print ("\nPrimeiro objeto gravado ",obj)
+obj.name = "WORD11"
+Fatura_Reserva.update(getattr(obj, cl.att[0]))
+
+cl.read(filename + 'hotel.db')
+
+print("\nobjectos gravados")    
+for code in cl.lst:
+    print(cl.obj[code])
