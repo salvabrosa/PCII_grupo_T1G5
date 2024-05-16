ec2c664173effc327bd03bd2d0a2359ee4b2c396
César <up202305778@up.pt>
2024-05-16 18:02:47 +0100

    att classe quarto

--- classes/quarto.py
+++ classes/quarto.py
@@ -1,5 +1,6 @@
 from classes.gclass import Gclass
 from classes.hotel import Hotel
+from classes.tiposquarto import TiposQuarto
 import datetime
 
 class Quarto(Gclass):
@@ -10,21 +11,26 @@ class Quarto(Gclass):
     auto_number = 0
     nkey = 1
     
-    att = ['_codigo','_cod_hotel','_preco_noite','_estado_reserva']
+    att = ['_codigo','_cod_hotel','_preco_noite','_tipoquarto','_estado_reserva']
     header = 'Quarto'
-    des = ['Codigo do quarto','Codigo do hotel','Preço por noite','Estado da reserva']
+    des = ['Codigo do quarto','Codigo do hotel','_tipoquarto','Estado da reserva']
     
-    def __init__(self, codigo, cod_hotel, preco_noite, estado_reserva):
+    def __init__(self, codigo, cod_hotel, preco_noite, tipoquarto,  estado_reserva):
         super().__init__()
         
         if cod_hotel in Hotel.lst:
-            self._codigo = codigo
-            self._cod_hotel = cod_hotel
-            self._preco_noite = preco_noite
-            self._estado_reserva=estado_reserva
+            if tipoquarto in TiposQuarto.lst:
+            
+                self._codigo = codigo
+                self._cod_hotel = cod_hotel
+                self._preco_noite = preco_noite
+                self._tipoquarto = tipoquarto
+                self._estado_reserva=estado_reserva
         
-            Quarto.obj[codigo] = self
-            Quarto.lst.append(codigo)
+                Quarto.obj[codigo] = self
+                Quarto.lst.append(codigo)
+            else:
+                print('Tipos ', tipoquarto, ' not found')
         else:
             print('Hotel ', cod_hotel, ' not found')
             
@@ -37,6 +43,9 @@ class Quarto(Gclass):
     @property 
     def preco_noite(self):
         return self._preco_noite
+    @property
+    def tipoquarto(self):
+        return self._tipoquarto
     @property 
     def estado_reserva(self):
         return self._estado_reserva
@@ -53,37 +62,12 @@ class Quarto(Gclass):
     @preco_noite.setter 
     def preco_noite(self, preco_noite):
         self._preco_noite = preco_noite
+    @tipoquarto.setter
+    def tipoquarto(self, tipoquarto):
+        if tipoquarto in TiposQuarto.lst:
+            self._tipoquarto = tipoquarto
+        else:
+            print('Tipos ', tipoquarto, ' not found')
     @estado_reserva.setter 
     def estado_reserva(self, estado_reserva):
         self._estado_reserva = estado_reserva
-        
-class TiposQuarto(Gclass):
-    obj = dict()
-    lst = list()
-    pos = 0
-    sortkey = ''
-    auto_number = 0
-    nkey = 1
-    
-    att = ['_codigo','_name']
-    header = 'Tipos'
-    des = ['Codigo','']
-    def __init__(self, codigo,name):
-        super().__init__()
-
-        self._codigo = codigo
-        self._name = name
-
-        TiposQuarto.obj[codigo] = self
-        TiposQuarto.lst.append(codigo)
-
-
-    @property
-    def codigo(self):
-        return self._codigo
-    @property
-    def name(self):
-        return self._name    
-    @name.setter
-    def name(self, name):
-        self._name = name
---
+++ classes/tiposquarto.py
@@ -0,0 +1,32 @@
+from classes.gclass import Gclass
+
+class TiposQuarto(Gclass):
+    obj = dict()
+    lst = list()
+    pos = 0
+    sortkey = ''
+    auto_number = 0
+    nkey = 1
+    
+    att = ['_codigo','_name']
+    header = 'Tipos'
+    des = ['Codigo','']
+    def __init__(self, codigo,name):
+        super().__init__()
+
+        self._codigo = codigo
+        self._name = name
+
+        TiposQuarto.obj[codigo] = self
+        TiposQuarto.lst.append(codigo)
+
+
+    @property
+    def codigo(self):
+        return self._codigo
+    @property
+    def name(self):
+        return self._name    
+    @name.setter
+    def name(self, name):
+        self._name = name
