fa5ddc9dc02b9fe9d423d2510e5d3905ce71c6b9
César <up202305778@up.pt>
2024-05-16 17:53:48 +0100

    tipos quartos

--- classes/quarto.py
+++ classes/quarto.py
@@ -56,3 +56,34 @@ class Quarto(Gclass):
     @estado_reserva.setter 
     def estado_reserva(self, estado_reserva):
         self._estado_reserva = estado_reserva
+        
+class TiposQuarto(Gclass):
+    obj = dict()
+    lst = list()
+    pos = 0
+    sortkey = ''
+    auto_number = 0
+    nkey = 1
+    
+    att = ['_codigo','_name']
+    header = 'Tipos'
+    des = ['Codigo','']
+    def __init__(self, codigo,name):
+        super().__init__()
+
+        self._codigo = codigo
+        self._name = name
+
+        TiposQuarto.obj[codigo] = self
+        TiposQuarto.lst.append(codigo)
+
+
+    @property
+    def codigo(self):
+        return self._codigo
+    @property
+    def name(self):
+        return self._name    
+    @name.setter
+    def name(self, name):
+        self._name = name
