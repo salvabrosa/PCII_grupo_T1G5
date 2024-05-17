0aba003b708b5e90c4f7481e84bda140a7b2b10a
César <up202305778@up.pt>
2024-05-17 15:09:11 +0100

    teste reservaquarto

--- testes/teste_reservaquarto.py
+++ testes/teste_reservaquarto.py
@@ -3,21 +3,23 @@ from datafile import filename
 from classes.hotel import Hotel
 from classes.quarto import Quarto
 from classes.reservaquarto import ReservaQuarto
+from classes.tiposquarto import TiposQuarto
 
 Hotel.read(filename + 'hotel.db')
+TiposQuarto.read(filename + 'hotel.db')
 Quarto.read(filename + 'hotel.db')
 ReservaQuarto.read(filename + 'hotel.db')
 
 cname = "ReservaQuarto"
 cl = eval(cname)
 
-obj = cl.from_string('rq1;2-01-2024;2-01-2024;Ativa;A100;12345')
+obj = cl.from_string('10000;2-01-2024;2-01-2024;Ativa;A100;12345')
 
 print("objeto sem estar gravado ",obj)
 
 cl.insert(getattr(obj,cl.att[0]))
 
-obj = cl.from_string('rq1;2-01-2024;2-01-2024;Ativa;A100;12345')
+obj = cl.from_string('20000;2-01-2024;2-01-2024;Ativa;A100;12345')
 cl.insert(getattr(obj,cl.att[0]))
 
 
